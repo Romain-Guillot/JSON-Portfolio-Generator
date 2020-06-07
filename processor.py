55247281c9ad9dadb8c9968c9f98bf7d66badbcf
Romain Guillot <rguillot42@gmail.com>
2020-06-07 19:24:19 -0400

    Automatically add generated file

--- processor.py
+++ processor.py
@@ -44,6 +44,19 @@ def prepareOutputDir(config) :
     copy_tree(ASSETS_DIR, OUTPUT_ASSET_DIR)
 
 
+    for filename in os.listdir(OUTPUT_DIR):
+        file_path = os.path.join(OUTPUT_DIR, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                # os.unlink(file_path)
+                print(file_path)
+            elif os.path.isdir(file_path):
+                print(file_path)
+                # shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
 def publishOnGithub(config) :
     """
     Publish the output directory in the default git remote
