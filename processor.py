04f28d7561666968872cc80833ebe3ba15df7d06
Romain Guillot <rguillot42@gmail.com>
2020-06-10 11:01:10 -0400

    Restructure project

--- processor.py
+++ processor.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 
 import json
 from distutils.dir_util import copy_tree
@@ -13,91 +13,107 @@ from lib.jinja_service import JinjaService
 from lib.config import Config
 
 
-def loadData(config) :
-    """.
-    return the data dictionnary from the json file.
-    append the config dictionnary to the result, available through the `config`
-    key
+class Processor :
     """
-    dataFilename = config['data']
-    with open(dataFilename, 'r') as data_file :
-        data = json.loads(data_file.read())
-        return {'config': config.config, **data}
-
-
-def prepareOutputDir(config) :
+    Manage all process to generate the portfolio
+    Baiscally a sequence of calls to the different services (see lib/)
     """
-    (re)create the output directories
-    copy the assets dir into the output dir
-    """
-    OUTPUT_DIR = config['output']
-    ASSETS_DIR = config['src']['static']
-    
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
-
-    for filename in os.listdir(OUTPUT_DIR):
-        file_path = os.path.join(OUTPUT_DIR, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path) and filename != ".git":
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-    OUTPUT_ASSET_DIR = os.path.join(OUTPUT_DIR, ASSETS_DIR)
-    if not os.path.exists(OUTPUT_ASSET_DIR):
-        os.makedirs(OUTPUT_ASSET_DIR)
-    copy_tree(ASSETS_DIR, OUTPUT_ASSET_DIR)
+    def __init__(self):
+        self.config = Config("config.yml")
+        self.data = self.loadData()
+        SRC_DIR = sys.path[0]
+        self.OUTPUT_DIR = self.config['output']
+        self.STATIC_DIR = os.path.join(SRC_DIR, "static")
+        self.TEMPLATE_DIR = os.path.join(SRC_DIR, "templates")
+        self.process()
 
 
+    def process(self):
+        self.prepareOutputDir()
+        self.renderPages()
+        self.buildStyle()
+        self.publishOnGithub()
+        self.buildPDF()
 
 
-
-def publishOnGithub(config) :
-    """
-    Publish the output directory in the default git remote
-    """
-    OUTPUT_DIR = config['output']
-    publicationRequired = config['git_publish']
-    if publicationRequired :
-        GithubService(OUTPUT_DIR).publish()
-
-
-def renderPages(config, data) :
-    """
-    Built the html of all pages : index, resume, projects and one page for
-    each project
-    """
-    OUTPUT_DIR = config['output']
-    TEMPLATE_DIR = config['src']['templates']
-
-    jinjaService = JinjaService(TEMPLATE_DIR, OUTPUT_DIR)
-    jinjaService.renderPage("index", None, data, data["meta"]["description-index"])
-    jinjaService.renderPage("resume", "Résumé", data, data["meta"]["description-resume"])
-    jinjaService.renderPage("projects", "Projects", data, data["meta"]["description-projects"])
-
-    for project in data['projects'] :
-        project_data = {'project': project, **data}
-        jinjaService.renderPage('project_details', project['title'], project_data, project['description'], project['id'])
+    def loadData(self) :
+        """.
+        return the data dictionnary from the json file.
+        append the config dictionnary to the result, available through the `config`
+        key
+        """
+        dataFilename = self.config['data']
+        with open(dataFilename, 'r') as data_file :
+            data = json.loads(data_file.read())
+            return {'config': self.config.config, **data}
 
 
+    def prepareOutputDir(self) :
+        """
+        (re)create the output directories
+        copy the STATIC dir into the output dir
+        """
+        if not os.path.exists(self.OUTPUT_DIR):
+            os.makedirs(self.OUTPUT_DIR)
 
-def buildStyle(config) :
-    ASSETS_DIR = config['src']['static']
-    OUTPUT_DIR = config['output']
-    SassService(ASSETS_DIR, OUTPUT_DIR).compile()
+        for filename in os.listdir(self.OUTPUT_DIR):
+            file_path = os.path.join(self.OUTPUT_DIR, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path) and filename != ".git":
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        
+        OUTPUT_STATIC_DIR = os.path.join(self.OUTPUT_DIR, "static")
+        os.makedirs(OUTPUT_STATIC_DIR)
+        copy_tree(self.STATIC_DIR, OUTPUT_STATIC_DIR)
+
+        USER_ASSET_DIR = self.config['assets_dir']
+        OUTPUT_USER_ASSET_DIR = os.path.join(self.OUTPUT_DIR, USER_ASSET_DIR)
+        os.makedirs(OUTPUT_USER_ASSET_DIR)
+        copy_tree(USER_ASSET_DIR, OUTPUT_USER_ASSET_DIR)
+
+
+    def publishOnGithub(self) :
+        """
+        Publish the output directory in the default git remote
+        """
+        publicationRequired = self.config['git_publish']
+        if publicationRequired :
+            GithubService(self.OUTPUT_DIR).publish()
+
+
+    def renderPages(self) :
+        """
+        Built the html of all pages : index, resume, projects and one page for
+        each project
+        """
+        jinjaService = JinjaService(self.TEMPLATE_DIR, self.OUTPUT_DIR)
+        jinjaService.renderPage("index", None, self.data, self.data["meta"]["description-index"])
+        jinjaService.renderPage("resume", "Résumé", self.data, self.data["meta"]["description-resume"])
+        jinjaService.renderPage("projects", "Projects", self.data, self.data["meta"]["description-projects"])
+        for project in self.data['projects'] :
+            project_data = {'project': project, **self.data}
+            jinjaService.renderPage('project_details', project['title'], project_data, project['description'], project['id'])
+
+
+    def buildStyle(self) :
+        SassService(self.STATIC_DIR, self.OUTPUT_DIR).compile()
+
+
+    def buildPDF(self) :
+        PDFService("/usr/bin/google-chrome", "file:///home/ob/Documents/projects/Active/Portfolio/build/resume.html").build()
+
 
 
 if __name__ == '__main__' :
-    # logging.getLogger().setLevel(logging.INFO)
-    config = Config("config.yml")
-    prepareOutputDir(config)
-    data = loadData(config)
-    renderPages(config, data)
-    buildStyle(config)
-    publishOnGithub(config)
-    
-    PDFService("/usr/bin/google-chrome", "file:///home/ob/Documents/projects/Active/Portfolio/build/resume.html").build()
+    logging.getLogger().setLevel(logging.INFO)
+    base_directory = sys.argv[1];
+    os.chdir(base_directory)
+    if len(sys.argv) < 2 :
+        logging.error("Usage : preprocessor YOUR_DIRECTORY\nwith YOUT_DIRECTORY: the directory that contains the config file, your data file, your STATIC, etc.")
+        sys.exit(1)
+    Processor()
     
